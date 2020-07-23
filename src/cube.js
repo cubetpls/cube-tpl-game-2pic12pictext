8da2657905992366944746cc94b338b09ce39524
刘艳 <liuyan-iri@liuyandeMacBook-Pro.local>
2020-07-23 16:06:31 +0800

    update

--- src/cube.js
+++ src/cube.js
@@ -1,25 +1,10 @@
-import CubeCarousel from './components/Carousel/index.js'
-
 export default {
   data : {
     loading: true,
     carouselList: [],
-    inforList: [],
-    carouselTpl: [
-      '{{#list item as item2 by item2_index}}',
-        '<a href="{{item2.link}}" target="_blank">',
-          '<img src="{{item2.img | clipImage:\'180_100_75\'}}" width="180" height="100" alt="">',
-          '<p class="ext">',
-            '<span class="bg"></span>',
-            '<span class="title">{{item2.title}}</span>',
-          '</p>',
-        '</a>',
-        '{{/list}}',
-    ].join('')
+    inforList: []
   },
   onLoad(){
-    /*cube 被加载之后 */
-    this.createComponent(CubeCarousel)
   },
   onReady(){
     /*cube 被添加到页面之后 */
@@ -29,7 +14,7 @@ export default {
         this.setData({
           loading: false
         })
-        if (res && res.errno === 0 && res.data) {
+        if (res && res.errno === 1 && res.data) {
           const data = res.data
           this.handleData(data)
           this.setStorage({
--- src/cube.tpl
+++ src/cube.tpl
@@ -4,12 +4,30 @@
   {{/if}}
 
   <div class="carousel-box">
-    <cube-carousel
-      list="{{carouselList}}"
-      slot="{{carouselTpl}}"
+    <Carousel
+      interval="3000"
       width="368"
-      height="100">
-    </cube-carousel>
+      height="100"
+      change={{this.change}}
+      btns="hover"
+      autoplay={{true}}
+      class="cube-test">
+      {{#list carouselList as item by item_index}}
+        <Carousel.Item>
+          <div class="carousel-item gclearfix">
+          {{#list item as item2 by item2_index}}
+            <a href="{{item2.link}}" class="item" target="_blank">
+              <img src="{{item2.img | clipImage:'180_100_75'}}" width="180" height="100" alt="">
+              <p class="ext">
+                <span class="bg"></span>
+                <span class="title">{{item2.title}}</span>
+              </p>
+            </a>
+          {{/list}}
+          </div>
+        </Carousel.Item>
+		  {{/list}}
+	</Carousel>
   </div>
 
   <div class="infor-box">
